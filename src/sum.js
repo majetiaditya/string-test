4c83f58c384919e6e62005524849ba2c86d4e7af
homesh65 <majeti.nagahomesh@gmail.com>
2025-01-09 15:32:44 +0530

    added try catch blocks

--- src/sum.js
+++ src/sum.js
@@ -1,45 +1,53 @@
 export default function sum(numbers) {
-  if (typeof numbers !== 'string')
-    return `Input must be a string`
+  try {
+    if (typeof numbers !== 'string') throw new Error('Input must be a string');
+    
+    const numbersLength = numbers.length;
+    if (numbersLength === 0) return 0;
+    if (numbersLength === 1) return Number(numbers);
   
-  const numbersLength = numbers.length;
-  if (numbersLength === 0) return 0;
-  if (numbersLength === 1) return Number(numbers);
- 
-  let sum = 0;
-  let delimiter = ',';
-  if (numbers.includes('//') && numbers.includes('\n')) {
+    let sum = 0;
+    let delimiter = ',';
+    if (numbers.includes('//') && numbers.includes('\n')) {
       delimiter = numbers.charAt(2);
       numbers = numbers.substring(4, numbersLength);
-  }
- 
-  let negativeNumber = "";
-  let input = numbers.replaceAll('\n', ',').split(delimiter);
-  input = input.forEach(num => {
+    }
+  
+    let negativeNumber = "";
+    let input = numbers.replaceAll('\n', ',').split(delimiter);
+    input = input.forEach(num => {
       if (num < 0)
-          negativeNumber = `${negativeNumber}${num}, `
+        negativeNumber = `${negativeNumber}${num}, `;
+
       sum += Number(num);
-  });
+    });
 
-  if (negativeNumber.length > 0)
-      return `negative numbers not allowed ${negativeNumber.substring(0, negativeNumber.length - 2)}`;
+    if (negativeNumber.length > 0)
+      throw new Error(`negative numbers not allowed ${negativeNumber.substring(0, negativeNumber.length - 2)}`);
 
-  return sum;
+    return sum;
+  } catch (error) {
+    return error.message;
+  }
 }
 
-const res1 = sum("");
-const res2 = sum("1");
-const res3 = sum("1,5");
-const res4 = sum("1\n2,3");
-const res5 = sum("//;\n1;2");
-const res6 = sum("-1,5\n7");
-const res7 = sum("-1,5,-7");
+
+const res1 = sum([]);
+const res2 = sum("");
+const res3 = sum("1");
+const res4 = sum("1,5");
+const res5 = sum("1\n2,3");
+const res6 = sum("//;\n1;2");
+const res7 = sum("-1,5");
+const res8 = sum("-1,5\n7");
+const res9 = sum("-1,5,-7");
 
 console.log("res1 :", res1);
 console.log("res2 :", res2);
 console.log("res3 :", res3);
 console.log("res4 :", res4);
 console.log("res5 :", res5);
-
 console.log("res6 :", res6);
 console.log("res7 :", res7);
+console.log("res8 :", res8);
+console.log("res9 :", res9);
--- test/sum.test.js
+++ test/sum.test.js
@@ -3,7 +3,11 @@ import sum from "../src/sum.js";
 
 describe("Sum Function", () => {
   it("should throw an error if input is an array", () => {
-    expect(sum([])).to.equal("Input must be a string");
+    try {
+      sum([])
+    } catch (error) {
+      expect(error.message).to.equal("Input must be a string");
+    }
   });
 
   it("should return 0 for an empty string", () => {
@@ -11,11 +15,11 @@ describe("Sum Function", () => {
   });
 
   it("should return the number itself if the array has one number", () => {
-    expect(sum("5")).to.equal(5);
+    expect(sum("1")).to.equal(1);
   });
 
   it("should return the correct sum for multiple numbers", () => {
-    expect(sum("1,2,3,4")).to.equal(10);
+    expect(sum("1,5")).to.equal(6);
   });
 
   it("should return the correct sum for multiple numbers with new line", () => {
@@ -27,14 +31,26 @@ describe("Sum Function", () => {
   });
 
   it("should return an error message when string contains negative number", () => {
-    expect(sum("-1,5")).to.equal('negative numbers not allowed -1');
+    try {
+      sum("-1,5")
+    } catch (error) {
+      expect(error.message).to.equal("negative numbers not allowed -1");
+    }
   });
 
   it("should return an error message when string contains negative number with new line", () => {
-    expect(sum("-1,5\n7")).to.equal('negative numbers not allowed -1');
+    try {
+      sum("-1,5\n7")
+    } catch (error) {
+      expect(error.message).to.equal("negative numbers not allowed -1");
+    }
   });
 
   it("should return an error message when string contains negative numbers", () => {
-    expect(sum("-1,5,-7")).to.equal('negative numbers not allowed -1, -7');
-  }); 
+    try {
+      sum("-1,5,-7")
+    } catch (error) {
+      expect(error.message).to.equal("negative numbers not allowed -1, -7");
+    }
+  });
 });
